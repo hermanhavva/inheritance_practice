69dfe6c7b5a55e391ffe875b44167ef7353ac4e3
Herman Havva <108483440+hermanhavva@users.noreply.github.com>
2024-07-08 22:12:16 +0300

    Update Source.cpp

--- Inheritance_practice/Source.cpp
+++ Inheritance_practice/Source.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main ()
+void CatSay()
 {
-	
+	cout << "meow" << endl;
+}
+
+void DogSay()
+{
+	cout << "ruff" << endl;
+}
+
+int main()
+{
+	DogSay();
+	CatSay();
 }
